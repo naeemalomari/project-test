2863a7706fcf2cec70e4c44ad9ca96cb79cd0dde
naeemalomari <naeemalomari96@gmail.com>
2021-08-14 23:47:34 +0300

    done with header

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Navbar,Container,Nav,NavDropdown,Col,Row} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 class Header extends React.Component {
 
     render(){
@@ -11,18 +12,31 @@ class Header extends React.Component {
         return (
 
 <>
-  <Navbar bg="dark" variant="dark" >
+  <Navbar bg="dark" variant="dark" className='py-4'>
     <Container>
-    <Navbar.Brand href="#home">ZOO PLANET</Navbar.Brand>
+    <Container>
+      <Navbar.Brand href="home" className='mt-5'>
+        <img
+          alt=""
+          src="https://trello.com/1/cards/6117ccdbc5aaea8ec9c03df9/attachments/6117cd9494bdc903adbbf27b/download/planetPets-01.png"
+          width="100"
+          height="100"
+          className="d-inline-block "
+        />
+      ZOO PLANET 
+      </Navbar.Brand>
+    </Container>
+
     <Nav 
       className="mr-auto my-2 my-lg-0">
-      <Nav.Link href="#home"> Home </Nav.Link>
-      <Nav.Link href="#features">Blog</Nav.Link>
-      <NavDropdown title="JOIN US " id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Adoption</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Donation</NavDropdown.Item>
+      <Nav.Link href="home" > Home </Nav.Link>
+      <Nav.Link href="blog">Blog</Nav.Link>
+      <NavDropdown title="Adopt/Donate" id="basic-nav-dropdown">
+          <NavDropdown.Item href="Adopt an Animal">AdoptAnAnimal</NavDropdown.Item>
+          <NavDropdown.Item href="Contribute">Contribute</NavDropdown.Item>
+          <NavDropdown.Item href="Give Up Your Animal">GiveUpYourAnimal</NavDropdown.Item>
         </NavDropdown>
-      <Nav.Link href="#pricing"> About Us </Nav.Link>
+      <Nav.Link href="aboutus">AboutUs</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
